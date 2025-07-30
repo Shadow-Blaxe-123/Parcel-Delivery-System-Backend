d6c8429d247c9a13aff75da72fd5c897ced8f284
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-07-30 23:12:07 +0600

    app.ts file done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,19 @@
 import express, { Application } from "express";
-
+import cors from "cors";
+import notFound from "./middleware/404NotFound";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 const app: Application = express();
 
+// 3rd party middleware
+app.use(express.json());
+app.use(cors);
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World" });
+});
+
+// Error handlers
+app.use(globalErrorHandler);
+app.use(notFound);
 export default app;
---
+++ src/middleware/globalErrorHandler.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextFunction, Request, Response } from "express";
+import AppError from "../utils/AppError";
+import { env } from "../config/env";
+
+function globalErrorHandler(
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  let statuscode = 500;
+  let message = "Something went wrong";
+  //   let errorSources: ErrorSources[] | undefined = [];
+
+  if (env.NODE_ENV === "development") {
+    console.log(err);
+  }
+
+  if (err instanceof AppError) {
+    statuscode = err.statusCode;
+    message = err.message;
+  } else if (err instanceof Error) {
+    message = err.message;
+    statuscode = 500;
+  }
+
+  res.status(statuscode).json({
+    success: false,
+    message,
+    // errorSources,
+    err: env.NODE_ENV === "development" ? err : null,
+    stack: env.NODE_ENV === "development" ? err.stack : null,
+  });
+  next();
+}
+
+export default globalErrorHandler;
