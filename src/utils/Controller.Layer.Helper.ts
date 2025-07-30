44109307458d070e10575acc6e4741021c1179da
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-07-31 00:50:27 +0600

    Some Utils

---
+++ src/utils/Controller.Layer.Helper.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextFunction, Request, Response } from "express";
+import { env } from "../config/env";
+
+type Parameter = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<void>;
+
+const catchPromise =
+  (fn: Parameter) => (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err: any) => {
+      if (env.NODE_ENV === "development") {
+        console.log(err);
+      }
+      next(err);
+    });
+  };
+
+export default catchPromise;
---
+++ src/utils/sendResponse.ts
@@ -0,0 +1,27 @@
+import { Response } from "express";
+
+interface TMeta {
+  total: number;
+  page: number;
+  limit: number;
+  totalPages: number;
+}
+
+interface IResponse<T> {
+  statusCode: number;
+  message: string;
+  data: T;
+  meta?: TMeta;
+}
+
+const sendResponse = <T>(res: Response, data: IResponse<T>) => {
+  res.status(data.statusCode).json({
+    statusCode: data.statusCode,
+    success: true,
+    message: data.message,
+    data: data.data,
+    meta: data.meta,
+  });
+};
+
+export default sendResponse;
