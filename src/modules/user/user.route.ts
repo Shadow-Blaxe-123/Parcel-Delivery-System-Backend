9e18cc17649de8959885828607c6ff285b75dbab
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-08-02 15:25:18 +0600

    Zod Validation added to user.

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,9 +1,15 @@
 import { Request, Response, Router } from "express";
 import { UserController } from "./user.controller";
+import validateRequest from "../../middleware/validateRequest";
+import { userValidation } from "./user.validation";
 
 const router = Router();
 router.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Hello User" });
 });
-router.post("/create", UserController.userCreate);
+router.post(
+  "/create",
+  validateRequest(userValidation.userCreateSchema),
+  UserController.userCreate
+);
 export const UserRoutes = router;
--- src/modules/user/user.validation.ts
+++ src/modules/user/user.validation.ts
@@ -4,9 +4,20 @@ import { UserRole } from "./user.interface";
 const userCreateSchema = z.object({
   name: z.string(),
   email: z.email(),
-  password: z.string(),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .regex(/^(?=.*[A-Z])/, {
+      error: "Password must contain at least 1 uppercase letter.",
+    })
+    .regex(/^(?=.*[!@#$%^&*])/, {
+      error: "Password must contain at least 1 special character.",
+    })
+    .regex(/^(?=.*\d)/, {
+      error: "Password must contain at least 1 number.",
+    }),
   address: z.string(),
   role: z.enum([UserRole.RECEIVER, UserRole.SENDER]),
 });
 
-export const userCreateValidation = { userCreateSchema };
+export const userValidation = { userCreateSchema };
